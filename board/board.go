f8f8e5381d8fa8870b731613746cc249ea86dfbc
Brandon Piña <brandon.pina@invista.com>
2025-03-23 05:10:45 +0000

    some structural changes

--- board/board.go
+++ board/board.go
@@ -41,11 +41,29 @@ type Board struct {
 	Cells     [BOARD_DIM][BOARD_DIM]Cell
 	ColTotals [BOARD_DIM]int
 	RowTotals [BOARD_DIM]int
+	symbols   [BOARD_DIM][BOARD_DIM]z3.Int
+	ctx       *z3.Context
+	slv       *z3.Solver
 }
 
 func NewBoard(name string) Board {
 	return Board{
 		Name: name,
+	}
+}
+
+func (b *Board) checkcols() {
+	for x := range BOARD_DIM {
+		constname := fmt.Sprintf("col_%d_sum_%d", x, b.ColTotals[x])
+		sum := b.ctx.IntConst(constname)
+		for y := range BOARD_DIM {
+			cond := b.symbols[y][x].Eq(b.intToConst(int(Wall)))
+			AddBoolToInt(&sum, &cond)
+		}
+		assertion_const := b.ctx.BoolConst(fmt.Sprintf("%s-satisfied", constname))
+		assertion := assertion_const.Eq(sum.Eq(b.intToConst(1)))
+		log.Debug(assertion)
+		b.slv.Assert(assertion)
 	}
 }
 
@@ -88,15 +106,10 @@ func (b Board) String() string {
 func (b Board) Solve() (*Board, error) {
 	log.Infof("solving %s", b.Name)
 	cfg := z3.NewContextConfig()
-	ctx := z3.NewContext(cfg)
-	slv := z3.NewSolver(ctx)
+	b.ctx = z3.NewContext(cfg)
+	b.slv = z3.NewSolver(b.ctx)
 
-	// constant values
-	//unk := ctx.FromInt(int64(Unknown), ctx.IntSort()).(z3.Int)
-	//max_sym := ctx.FromInt(int64(len(EntTranslations)), ctx.IntSort()).(z3.Int)
-	wall := ctx.FromInt(int64(Wall), ctx.IntSort()).(z3.Int)
-	space := ctx.FromInt(int64(Space), ctx.IntSort()).(z3.Int)
-	var symbols [BOARD_DIM][BOARD_DIM]z3.Int
+	// setting up the board
 	for x := range BOARD_DIM {
 		for y := range BOARD_DIM {
 			c := b.Cells[y][x]
@@ -104,84 +117,73 @@ func (b Board) Solve() (*Board, error) {
 			var sym z3.Int
 			switch c {
 			case Unknown:
-				sym = ctx.IntConst(constname)
-				space_or_wall := sym.Eq(wall).Xor(sym.Eq(space))
-				//space_or_wall := sym.Eq(space).Or(sym.Eq(wall))
-				slv.Assert(space_or_wall)
+				sym = b.ctx.IntConst(constname)
+				space_or_wall := sym.Eq(b.intToConst(int(Wall))).Xor(sym.Eq(b.intToConst(int(Space))))
+				b.slv.Assert(space_or_wall)
 				log.Debugf("%d,%d %s", x, y, space_or_wall)
 			default:
-				sym = ctx.FromInt(int64(c), ctx.IntSort()).(z3.Int)
+				sym = b.intToConst(int(c))
 			}
 
-			symbols[y][x] = sym
-			//log.Tracef("created symbol %s", sym)
+			b.symbols[y][x] = sym
+			log.Debugf("created symbol %s", sym)
 		}
 	}
 
-	for x := range BOARD_DIM {
-		for y := range BOARD_DIM {
-			c := b.Cells[y][x]
-			constname := fmt.Sprintf("cell_%d_%d", x, y)
+	/*
+		for x := range BOARD_DIM {
+			for y := range BOARD_DIM {
+				c := b.Cells[y][x]
+				constname := fmt.Sprintf("cell_%d_%d", x, y)
 
-			switch c {
-			case Monster:
-				//hallway_constraint := ctx.BoolConst(fmt.Sprintf("%s_hallway_constraint", constname))
-				hallway_sum := ctx.IntConst(fmt.Sprintf("%s_hallway_sum", constname))
-				dim := BOARD_DIM - 1
-				if x < dim {
-					//hallway_constraint.Implies()
-					cond := symbols[y][x+1].Eq(space)
-					AddBoolToInt(&hallway_sum, &cond)
-					//hallway_constraint = hallway_constraint.Xor(cond)
+				switch c {
+				case Monster:
+					//hallway_constraint := ctx.BoolConst(fmt.Sprintf("%s_hallway_constraint", constname))
+					hallway_sum := ctx.IntConst(fmt.Sprintf("%s_hallway_sum", constname))
+					dim := BOARD_DIM - 1
+					if x < dim {
+						//hallway_constraint.Implies()
+						cond := symbols[y][x+1].Eq(space)
+						AddBoolToInt(&hallway_sum, &cond)
+						//hallway_constraint = hallway_constraint.Xor(cond)
+					}
+					if x > 0 {
+						cond := symbols[y][x-1].Eq(space)
+						AddBoolToInt(&hallway_sum, &cond)
+						//hallway_constraint = hallway_constraint.Xor(cond)
+					}
+					if y < dim {
+						cond := symbols[y+1][x].Eq(space)
+						AddBoolToInt(&hallway_sum, &cond)
+						//hallway_constraint = hallway_constraint.Xor(cond)
+					}
+					if y > 0 {
+						cond := symbols[y-1][x].Eq(space)
+						AddBoolToInt(&hallway_sum, &cond)
+						//hallway_constraint = hallway_constraint.Xor(cond)
+					}
+					//log.Debug(hallway_constraint)
+					//slv.Assert(hallway_constraint)
+					slv.Push()
+					slv.Assert(hallway_sum.Eq(ctx.FromInt(1, ctx.IntSort()).(z3.Int)))
 				}
-				if x > 0 {
-					cond := symbols[y][x-1].Eq(space)
-					AddBoolToInt(&hallway_sum, &cond)
-					//hallway_constraint = hallway_constraint.Xor(cond)
-				}
-				if y < dim {
-					cond := symbols[y+1][x].Eq(space)
-					AddBoolToInt(&hallway_sum, &cond)
-					//hallway_constraint = hallway_constraint.Xor(cond)
-				}
-				if y > 0 {
-					cond := symbols[y-1][x].Eq(space)
-					AddBoolToInt(&hallway_sum, &cond)
-					//hallway_constraint = hallway_constraint.Xor(cond)
-				}
-				//log.Debug(hallway_constraint)
-				//slv.Assert(hallway_constraint)
-				slv.Push()
-				slv.Assert(hallway_sum.Eq(ctx.FromInt(1, ctx.IntSort()).(z3.Int)))
 			}
-		}
-	}
+		}*/
 
-	for col_x := range BOARD_DIM {
-
-		col_sum := ctx.IntConst(fmt.Sprintf("col_%d_sum", col_x))
-		for y := range BOARD_DIM {
-			iswall := symbols[y][col_x].Eq(wall)
-			AddBoolToInt(&col_sum, &iswall)
-		}
-		tot := ctx.FromInt(int64(b.ColTotals[col_x]), ctx.IntSort()).(z3.Int)
-		assertion := col_sum.Eq(tot)
-		log.Debug(assertion)
-		slv.Assert(assertion)
-	}
-
-	sat, err := slv.Check()
+	b.checkcols()
+	log.Info(b.slv)
+	sat, err := b.slv.Check()
 	if !sat {
 		return nil, err
 	}
-	m := slv.Model()
-	//fmt.Println(m)
+	m := b.slv.Model()
+	fmt.Println(m)
 	nb := NewBoard(fmt.Sprintf("%s (solved)", b.Name))
 	nb.SetColTotals(b.ColTotals)
 	nb.SetRowTotals(b.RowTotals)
 	for x := range BOARD_DIM {
 		for y := range BOARD_DIM {
-			v := m.Eval(symbols[y][x], true).(z3.Int)
+			v := m.Eval(b.symbols[y][x], true).(z3.Int)
 			val, _, _ := v.AsInt64()
 			nb.Cells[y][x] = Cell(val)
 		}
@@ -193,7 +195,16 @@ func (b Board) Solve() (*Board, error) {
 func AddBoolToInt(i *z3.Int, b *z3.Bool) {
 	//log.Debugf("Adding %s to %s", i, b)
 	c := i.Context()
+
 	bi := b.IfThenElse(c.FromInt(1, c.IntSort()), c.FromInt(0, c.IntSort()))
 	tmp := i.Add(bi.(z3.Int))
 	*i = tmp
+}
+
+// Create an int const to represent a specific cell variant
+func (b Board) intToConst(c int) z3.Int {
+	if b.ctx == nil || b.slv == nil {
+		log.Fatalf("Board %s does not have configured context or solver", b.Name)
+	}
+	return b.ctx.FromInt(int64(c), b.ctx.IntSort()).(z3.Int)
 }
